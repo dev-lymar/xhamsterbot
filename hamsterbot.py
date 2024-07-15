6e5f07aa86b5c4d598256174fd17f5f45e578d15
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-15 20:25:02 +0200

    Added buttons, change greeting

--- hamsterbot.py
+++ hamsterbot.py
@@ -1,5 +1,6 @@
 import os
 from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
+from telegram import ReplyKeyboardMarkup
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,7 +17,13 @@ def say_hi(update, context):
 
 def wake_up(update, context):
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='Dude, what are you doing here :/')
+    button = ReplyKeyboardMarkup([['Send me video ;)', 'Something new ']],resize_keyboard=True)
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Dude, what are you doing here... '
+        f'\nI know everything about you, {update.message.chat.first_name}!',
+        reply_markup=button
+    )
 
 
 updater.dispatcher.add_handler(CommandHandler('start', wake_up))
